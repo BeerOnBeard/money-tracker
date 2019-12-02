b7767fc9bf7bc8c21d7261ac50cfe4f4a08c7d95
BeerOnBeard <repoguy156+github@gmail.com>
2019-12-02 11:14:37 -0500

    Fixing css naming error in loading distractor

--- src/web/LoadingDistractor.js
+++ src/web/LoadingDistractor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function LoadingDistractor({ isActive }) {
   return (
-    <div className={'loadingDistractor' + (isActive ? ' loadingDistractor--active' : '' )}>
+    <div className={'loading-distractor' + (isActive ? ' loading-distractor--active' : '' )}>
       <span>Loading...</span>
     </div>
   );
